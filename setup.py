cd9f7634d889db6f644c316028cb97f21c28d2a7
jeremy <jeremy_ma@hotmail.com>
2016-08-15 18:16:15 -0700

    change location of source file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 from Cython.Build import cythonize
 
-sourcefiles = ['./gmmmc/fastgmm/fast_likelihood.pyx']
+sourcefiles = ['gmmmc/fastgmm/fast_likelihood.pyx']
 ext_modules = [Extension("fast_likelihood",
                           sourcefiles,
                           include_dirs = [np.get_include()],
@@ -16,10 +16,12 @@ setup(
     name='gmmmc',
     version='0.2.1.7',
     packages=['gmmmc', 'gmmmc.tests', 'gmmmc.priors', 'gmmmc.fastgmm', 'gmmmc.proposals'],
-    url='',
+    url='http://github.com/jeremy-ma/gmmmc',
     license='',
     author='Jeremy Ma',
     author_email='jeremy.ma@student.unsw.edu.au',
     description='Functions for drawing Monte Carlo samples from GMM parameter space',
+    download_url='https://github.com/jeremy-ma/gmmmc/tarball/0.1',
+    keywords = ['gmm', 'monte carlo', 'speech'],
     ext_modules= cythonize(ext_modules)
 )
